7ab1f16f437ca4dbed70aa5631040a32dab585b4
Michael Murry <michaelmurry49@gmail.com>
2020-04-29 21:34:21 -0700

    heroku test

---
+++ app/models/repopulate_user.rb
@@ -0,0 +1,2 @@
+class RepopulateUser < ApplicationRecord
+end
---
+++ test/models/repopulate_user_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RepopulateUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
