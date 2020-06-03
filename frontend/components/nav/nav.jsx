a853778e5f0b1c2ce7c06aa8a7a10f0746ed2620
Michael Murry <michaelmurry49@gmail.com>
2020-06-02 21:25:37 -0700

    add search glass

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, Button, Redirect } from "react-router-dom";
 import PopupContainer from "./popup_container";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 
 {/* <PopupContainer task={"Create a new Video"} />  */}
 
@@ -29,12 +31,13 @@ class Nav extends React.Component {
         // const {openPopup} = this.props
         return (
             <div className="nav">
+                {/* <div><FontAwesomeIcon icon={faBars} /></div> */}
                 <div className="nav-logo-plus-title">
                     <Link className="nav-logo-plus-title" to={"/"}><img className="nav-logo" src={window.smileURL} alt="BlueTube logo"/>BlueTube</Link>
                 </div>
                 <div className="nav-search-bar">
                     <input className="search-bar" value={this.state.filter} type="text" onChange={e => this.changeSearch(e)}/>
-                    <Link className="search-button" to={`/result/${this.state.filter}`}>Search</Link>
+                    <Link className="search-button" to={`/result/${this.state.filter}`}><FontAwesomeIcon icon={faSearch} /></Link>
                 </div>
                 <div className="right-nav">
                     <button className="camera-button" onClick={this.props.openPopup}><img className="nav-camera" src={window.cameraURL} alt="Camera logo" /></button>    
