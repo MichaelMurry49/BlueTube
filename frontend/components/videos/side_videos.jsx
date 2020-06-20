eb90196cbb0964a850605300ebfe5d8bfe853f47
Michael Murry <michaelmurry49@gmail.com>
2020-06-20 08:52:42 -0700

    select side videos randomly

--- frontend/components/videos/side_videos.jsx
+++ frontend/components/videos/side_videos.jsx
@@ -10,23 +10,25 @@ class SideVideos extends React.Component {
     componentDidMount(){
         this.props.fetchVideos();
     }
-    // randomVideos(videos, n){
-    //     let randVids = Object.values(videos).slice(0);
-    //     let sideVids = [];
-    //     for(let i = 0; i < videos.length && i < n; i++){
-    //         let rand = Math.floor(Math.random() * randVids.length);
-    //         sideVids.push(randVids[rand]);
-    //         randVids = randVids.slice(0, rand).concat(randVids.slice(rand+1));
-    //     }
-    //     return sideVids;
 
-    // }
+    randomVideos(videos, n){
+        debugger;
+        let randVids = Object.values(videos).slice(0);
+        let sideVids = [];
+        for(let i = 0; i < Object.values(videos).length && i < n; i++){
+            let rand = Math.floor(Math.random() * randVids.length);
+            sideVids.push(randVids[rand]);
+            randVids = randVids.slice(0, rand).concat(randVids.slice(rand+1));
+        }
+        return sideVids;
+
+    }
     render(){
         debugger;
         let {video} = this.props;
         return <div>
             <div>Up Next</div>
-            {this.props.videos ? Object.values(this.props.videos).slice(0,5).map(video => {
+            {this.props.videos ? this.randomVideos(this.props.videos,5).map(video => {
                 return(
                     <div className="sidepanel videoSearch" >
                         
