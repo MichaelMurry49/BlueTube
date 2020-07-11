86e0325b9563b7ff7d8ddb7e57383656b2625821
Michael Murry <michaelmurry49@gmail.com>
2020-07-11 11:22:50 -0700

    Make current user's like selection  show up as blue upon page refresh

--- frontend/components/likes/like.jsx
+++ frontend/components/likes/like.jsx
@@ -5,24 +5,10 @@ import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 class Like extends React.Component {
     constructor(props) {
         super(props);
-        this.likeClass = "gray";
-        this.dislikeClass = "gray";
     }
 
     componentDidMount(){
         this.props.fetchLikes();
-        debugger;
-        let uLike = Object.values(this.props.likes).filter(like => {
-            debugger;
-            return (like.likeableId === this.props.likeableId &&
-                like.likeableType === this.props.likeable &&
-                like.likerId === this.props.currentUser)
-        })
-        if(uLike.length > 0){
-            uLike[0].positiveLike ? this.likeClass = "blue" : this.dislikeClass = "blue"
-        }
-        
-        // this.props.obj.likes.forEach(likerId)
     }
 
     createLike(like) {
@@ -30,47 +16,50 @@ class Like extends React.Component {
             like.likeable_id === el.likeableId && like.likeable_type === el.likeableType);
         if (match.length > 0) {
             if (like.positive_like === match[0].positiveLike) {
-                like.positive_like ? this.likeClass = "gray " : this.dislikeClass = "gray ";
                 this.props.deleteLike(match[0].id);
             } else {
                 like.id = match[0].id;
                 like.liker_id = parseInt(like.liker_id, 10);
-                if(like.positive_like){
-                    this.likeClass = "blue ";
-                    this.dislikeClass = "gray ";
-                } else {
-                    this.likeClass = "gray ";
-                    this.dislikeClass = "blue ";
-                }
                 this.props.updateLike(like);
             }
         } else {
-            like.positive_like ? this.likeClass = "blue " : this.dislikeClass = "blue ";
             this.props.createLike(like);
         }
     }
 
     render(){
-        let {likeable, likeableId, likes} = this.props;
+        let {likeable, likeableId, likes, currentUser} = this.props;
+        let posLikes = Object.values(likes).filter(like => {
+            return (like.likeableId === likeableId &&
+                like.likeableType === likeable &&
+                like.positiveLike === true)
+        })
+        let negLikes = Object.values(likes).filter(like => {
+            return (like.likeableId === likeableId &&
+                like.likeableType === likeable &&
+                like.positiveLike === false)
+        })
         debugger;
         return(
             <div>
-                <button className={"like " + this.likeClass} onClick={() => this.createLike({ liker_id: this.props.currentUser, likeable_id: likeableId, likeable_type: likeable, positive_like: true })}>
-                    <FontAwesomeIcon icon={faThumbsUp} />
-                </button>
-                {Object.values(likes).filter(like => {
-                    return (like.likeableId === likeableId &&
-                        like.likeableType === likeable &&
-                        like.positiveLike === true)
-                }).length}
-                <button className={"like " + this.dislikeClass} onClick={() => this.createLike({ liker_id: this.props.currentUser, likeable_id: likeableId, likeable_type: likeable, positive_like: false })} >
-                    <FontAwesomeIcon icon={faThumbsDown} />
-                </button>
-                {Object.values(likes).filter(like => {
-                    return (like.likeableId === likeableId &&
-                        like.likeableType === likeable &&
-                        like.positiveLike === false)
-                }).length}
+                {posLikes.filter(like => like.likerId = parseInt(currentUser,10)).length > 0 ?
+                    <button className={"like " + "blue"} onClick={() => this.createLike({ liker_id: this.props.currentUser, likeable_id: likeableId, likeable_type: likeable, positive_like: true })}>
+                        <FontAwesomeIcon icon={faThumbsUp} />
+                    </button> :
+                    <button className={"like " + "gray"} onClick={() => this.createLike({ liker_id: this.props.currentUser, likeable_id: likeableId, likeable_type: likeable, positive_like: true })}>
+                        <FontAwesomeIcon icon={faThumbsUp} />
+                    </button>
+                }
+                {posLikes.length}  
+                {negLikes.filter(like => like.likerId = parseInt(currentUser, 10)).length > 0 ? 
+                    <button className={"like " + "blue"} onClick={() => this.createLike({ liker_id: this.props.currentUser, likeable_id: likeableId, likeable_type: likeable, positive_like: false })}>
+                        <FontAwesomeIcon icon={faThumbsUp} />
+                    </button> :
+                    <button className={"like " + "gray"} onClick={() => this.createLike({ liker_id: this.props.currentUser, likeable_id: likeableId, likeable_type: likeable, positive_like: false })}>
+                        <FontAwesomeIcon icon={faThumbsUp} />
+                    </button>
+                }
+                {negLikes.length}
             </div>
         ) 
     }
