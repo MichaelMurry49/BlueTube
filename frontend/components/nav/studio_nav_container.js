ef95674ef227eb82088f2c23590e6e9c9d1c12d7
Michael Murry <michaelmurry49@gmail.com>
2021-04-23 09:38:09 -0700

    remove dead code (studio-nav-container)

--- frontend/components/nav/studio_nav_container.js
+++ frontend/components/nav/studio_nav_container.js
@@ -1,11 +1,9 @@
 import { connect } from "react-redux";
 import { signOut } from "../../actions/session_actions";
 import { openPopup } from "../../actions/popup_actions";
-// import Nav from "./nav";
 import StudioNav from "./studio_nav";
 
 const mapStateToProps = state => {
-    debugger;
     return {
         currentUser: state.entities.users[state.session.currentUser],
         signedIn: Boolean(state.session.currentUser),
@@ -20,5 +18,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Nav)
 export default connect(mapStateToProps, mapDispatchToProps)(StudioNav)
