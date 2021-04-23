0cb45a9e738c5efa601b35b781bbd4aac6a002d8
Michael Murry <michaelmurry49@gmail.com>
2021-04-23 09:30:01 -0700

    remove deadcode/debuggers (nav container)

--- frontend/components/nav/nav_container.js
+++ frontend/components/nav/nav_container.js
@@ -1,11 +1,8 @@
 import { connect } from "react-redux";
 import { signOut } from "../../actions/session_actions";
-// import { openPopup } from "../../actions/popup_actions";
 import Nav from "./nav";
-import StudioNav from "./studio_nav";
 
 const mapStateToProps = state => {
-    debugger;
     return {
     currentUser: state.entities.users[state.session.currentUser],
     signedIn: Boolean(state.session.currentUser),
@@ -15,8 +12,6 @@ const mapDispatchToProps = dispatch => {
     return {
     signOut: () => dispatch(signOut()),
     postVideo: video => dispatch(postVideo(video)),
-    // openPopup: popup => dispatch(openPopup(popup)),
 }}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Nav)
-// export default connect(mapStateToProps, mapDispatchToProps)(StudioNav)
